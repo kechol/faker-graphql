6b83dc9d976e49a9ff90705e195fc357ff015ca7
Kazuyuki SUZUKI <kechol@users.noreply.github.com>
2022-02-16 04:32:41 +0900

    Generate schema.graphql types.d.ts

--- graphql/schema/index.ts
+++ graphql/schema/index.ts
@@ -1,8 +1,13 @@
+import * as path from "path";
 import { makeSchema } from "nexus";
-import * as HelloTypes from "./hello";
+import * as types from "./types";
 
 const schema = makeSchema({
-  types: [HelloTypes],
+  types,
+  outputs: {
+    schema: path.join(__dirname, "schema.graphql"),
+    typegen: path.join(__dirname, "types.d.ts"),
+  },
 });
 
 export default schema;
